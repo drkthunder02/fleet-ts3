0b8333fdde47cd4a39c44da1b16d21a6edcac0b3
drkthunder02 <mancuso.christopher@gmail.com>
2016-11-28 18:21:10 -0600

    secret uri

--- functions/crest.php
+++ functions/crest.php
@@ -4,7 +4,7 @@
 $clientid = '0ec717c4e93146a9913396bedb3720b5';
 $secret = 'tJkSWgWnoIGKmsRcoyOqZQP2xyTC8fJRWw4wB2ci';
 $callback_url = 'http://ts3.astrocomical.com/fleet-ts3/index.php';
-$useragent = 'TS3 Fleet Permissions';
+$useragent = 'TS3 Fleet Tracker';
 
 $redirect_uri = "https%3A%2F%2Fts3.astrocomical.com%2fleet-ts3%2Fauth.php";
 
