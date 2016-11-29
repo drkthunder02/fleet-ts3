2f9415719db15092e816969156f9d58e35f0b609
drkthunder02 <mancuso.christopher@gmail.com>
2016-11-28 18:33:46 -0600

    login.php

--- functions/crest.php
+++ functions/crest.php
@@ -6,6 +6,6 @@ $secret = 'tJkSWgWnoIGKmsRcoyOqZQP2xyTC8fJRWw4wB2ci';
 $callback_url = 'http://ts3.astrocomical.com/fleet-ts3/index.php';
 $useragent = 'TS3 Fleet Tracker';
 
-$redirect_uri = "https%3A%2F%2Fts3.astrocomical.com%2fleet-ts3%2Fauth.php";
+//$redirect_uri = "https%3A%2F%2Fts3.astrocomical.com%2fleet-ts3%2Fauth.php";
 
 ?>
