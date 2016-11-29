bbac06fe63c3acb7f1d800236b6385a2939fa4aa
drkthunder02 <mancuso.christopher@gmail.com>
2016-11-28 21:01:12 -0600

    fleet

--- functions/crest/fleetcontents.php
+++ functions/crest/fleetcontents.php
@@ -18,24 +18,5 @@ function FleetContents($fleetUrl, $fleetAuthToken, $useragent) {
     $result = curl_exec($ch);
     $response=json_decode($result);
     
-    /*
-    $url=$_SESSION['fleet_url'];
-    $ch = curl_init();
-    $header='Authorization: Bearer '.$_SESSION['fleet_auth_token'];
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array($header));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-    $result = curl_exec($ch);
-    $response=json_decode($result);
-    $url=$response->members->href;
-    curl_setopt($ch, CURLOPT_URL, $url);
-    $result = curl_exec($ch);
-    $response=json_decode($result);
-     * 
-     */
-    
     return $response;
 }
--- index.php
+++ index.php
@@ -16,25 +16,6 @@ if (!isset($_SESSION['fleet_url'])) {
     $fleetUrl = $_SESSION['fleet_url'];
     $fleetAuthToken = $_SESSION['fleet_auth_token'];    
     $response = FleetContents($fleetUrl, $fleetAuthToken, $useragent);
-    
-    /*
-    $url=$_SESSION['fleet_url'];
-    $ch = curl_init();
-    $header='Authorization: Bearer '.$_SESSION['fleet_auth_token'];
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array($header));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-    $result = curl_exec($ch);
-    $response=json_decode($result);
-    $url=$response->members->href;
-    curl_setopt($ch, CURLOPT_URL, $url);
-    $result = curl_exec($ch);
-    $response=json_decode($result);
-     * 
-     */
 }
 
 PrintFleetListingPage($response);
