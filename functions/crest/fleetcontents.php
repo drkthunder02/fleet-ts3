c4cc4245fdbbc19194bcf2c8511a2c75f464bf5e
drkthunder02 <mancuso.christopher@gmail.com>
2016-11-28 20:56:00 -0600

    fleet

--- functions/crest/fleetcontents.php
+++ functions/crest/fleetcontents.php
@@ -1,7 +1,7 @@
 <?php
 
 function FleetContents($fleetUrl, $fleetAuthToken, $useragent) {
-    
+    /*
     $url=$fleetUrl;
     $ch = curl_init();
     $header='Authorization: Bearer ' . $fleetAuthToken;
@@ -17,6 +17,24 @@ function FleetContents($fleetUrl, $fleetAuthToken, $useragent) {
     curl_setopt($ch, CURLOPT_URL, $url);
     $result = curl_exec($ch);
     $response = json_decode($result);
+     
+     */
+    
+    $url=$_SESSION['fleet_url'];
+    $ch = curl_init();
+    $header='Authorization: Bearer '.$_SESSION['fleet_auth_token'];
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array($header));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    $result = curl_exec($ch);
+    $response=json_decode($result);
+    $url=$response->members->href;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $result = curl_exec($ch);
+    $response=json_decode($result);
     
     return $response;
 }
--- index.php
+++ index.php
@@ -12,46 +12,25 @@ if (!isset($_SESSION['fleet_url'])) {
     //Stop performing code on the page until after the form is submitted.
     exit();
 } else {
-    //If we have a fleet_url, let's check for the fleet in the database.
-    //If it's not present then let's add the fleet to the database.
-    $fleetUrl = $_SESSION['fleet_url'];
-    $fleetAuthToken = $_SESSION['fleet_auth_token'];
-    $useragent="TS3 Fleet Permissions";
-    $response = FleetContents($fleetUrl, $fleetAuthToken, $useragent);
-    //Extract the fleet number from the fleetUrl
-    $fleetIdTemp = GetNumerics($fleetUrl);
-    //Store the value into fleet Id to be stored in the database
-    $fleetId = $fleetIdTemp[0];
-    
-    //Store the fleet in the database
-    $expiry = $_SESSION['fleet_expiry'];
-    //Store the fleet in the database
-    //StoreFleet($response, $fleetAuthToken, $fleetId, $expiry);
+    $url=$_SESSION['fleet_url'];
+    $ch = curl_init();
+    $header='Authorization: Bearer '.$_SESSION['fleet_auth_token'];
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array($header));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    $result = curl_exec($ch);
+    $response=json_decode($result);
+    $url=$response->members->href;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $result = curl_exec($ch);
+    $response=json_decode($result);
 }
 
-//PrintFleetListingPage($response);
+PrintFleetListingPage($response);
 
 ?>
 
-<html>
-<head><title>Fleet Tracker Example</title></head>
-<body>
-<table>
-<tr><th>Name</th><th>Location</th><th>Docked at</th><th>Ship</th></tr>
-<?php
-foreach ($response->items as $member) {
-    print "<tr><td>".$member->character->name."</td>";
-    print "<td>".$member->solarSystem->name."</td>";
-    if (isset($member->station)) {
-        print "<td>".$member->station->name."</td>";
-    } else {
-        print "<td>Undocked</td>";
-    }
-    print "<td>".$member->ship->name."</td>";
-    print "</tr>";
 
-}
-?>
-</table>
-</body>
-</html>
